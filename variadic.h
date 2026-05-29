ffa825310578ac80989564f12c203ed60704036b
antarcticite1 <osetinaaleksandra@gmail.com>
2026-05-29 17:09:12 +1000

    Add files via upload

---
+++ variadic.h
@@ -0,0 +1,8 @@
+#ifndef variadic_h
+#define variadic_h
+int sum(int count, ...);
+int max(int count, ...);
+int min(int count, ...);
+float average(int count, ...);
+float average_float(int count, ...);
+#endif
